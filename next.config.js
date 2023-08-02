c2ff7b0084e9de036f8ae8fb75467d92e5e1cb98
Jake Samuels <samuels242@googlemail.com>
2023-08-03 00:09:37 +0100

    Next config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ["lh3.googleusercontent.com", "asset.brandfetch.io"],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      };
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com", "asset.brandfetch.io"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
